3c42f1ec1735a1b3e02bdc0caae3836aefd7b164
VictorMMosqueraG <victormmosquerag@gmail.com>
2024-11-14 15:44:52 -0500

    refactor module about

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../components/navbar/navbar.component';
+import { AboutComponent } from '../about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    NavbarComponent
+    NavbarComponent,
+    AboutComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
