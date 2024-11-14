b12b1b9ea8ee837be177032f8992838eb36bba19
VictorMMosqueraG <victormmosquerag@gmail.com>
2024-11-14 16:30:51 -0500

    feature service was created

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { AboutComponent } from '../about/about.component';
+import { ServiceComponent } from "../service/service.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     NavbarComponent,
-    AboutComponent
-  ],
+    AboutComponent,
+    ServiceComponent
+],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
