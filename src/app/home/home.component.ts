f92a8ba322c0c542659497e5c3b36822f42ffb59
VictorMMosqueraG <victormmosquerag@gmail.com>
2024-11-29 13:24:04 -0500

    Refactor: connections between buttons and modules
    now all buttons redirect the user to the correct module

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavbarComponent } from '../components/navbar/navbar.component';
 import { AboutComponent } from '../about/about.component';
 import { ServiceComponent } from "../service/service.component";
@@ -21,4 +21,11 @@ import { ContactComponent } from '../contact/contact.component';
 })
 export default class HomeComponent {
 
+  //Creating a "nav" variable of type "NavbarComponent" and using the "viewChild" decorator to get an instance of NavbarComponent 
+  @ViewChild(NavbarComponent) nav!: NavbarComponent;
+
+  //NOTE: this method is for scrolling up or down through the components
+  scrollToSection(id: string) {
+    this.nav.scrollTo(id); //calling the "scrollTo" method of the NavbarComponent
+  }
 }
